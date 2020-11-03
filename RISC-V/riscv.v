e0781aadba0887c5ac6d71cd7c976dcd864b4243
Constantin Gierczak--Galle <constantin.gierczak-galle@ens.fr>
2020-11-03 10:02:19 +0100

    added RxD and TxD

--- RISC-V/riscv.v
+++ RISC-V/riscv.v
@@ -7,15 +7,11 @@
 
 module riscv(
         inout [15:0] ram_link,
-        inout [15:0] rom_link, 
-        input clk_in, 
-        output [2:0] led1, 
+        inout [15:0] rom_link,
+        input clk_in,
+        output [2:0] led1,
         output [2:0] led2);
   wire [31:0] rom_address;
-=======
-module riscv(ram_link, rom_link, clk_in, led1, led2, RxD, TxD);
-  wire [31:0] rom_adress;
->>>>>>> Stashed changes
   wire [31:0] rom_read;
   wire [31:0] ram_address;
   wire [31:0] ram_read;
@@ -26,18 +22,6 @@ module riscv(ram_link, rom_link, clk_in, led1, led2, RxD, TxD);
 
   wire [15:0] rom_link;
   wire [15:0] ram_link;
-
-<<<<<<< Updated upstream
-  core CORE(.ram_address(ram_address), .rom_address(rom_address), .data_in_ram(ram_read), .data_out_ram(ram_write), .data_in_rom(rom_read), .ram_enable_write(ram_write_enable), .vga_link(cpu_vga_link), .clk(clk));
-  ramctlr RAM(.ram_link(ram_link), .cpu_link_address(ram_address), .cpu_link_read(ram_read), .cpu_link_write(ram_write), .write_enable(ram_write_enable), .clk(clk));
-  romctlr ROM(.rom_link(rom_link), .cpu_link_read(rom_read), .cpu_link_address(rom_address), .clk(clk));
-=======
-  inout [15:0] ram_link;
-  inout [15:0] rom_link;
-  input clk_in;
-
-  output [2:0] led1;
-  output [2:0] led2;
 
   output TxD;
   input RxD;
@@ -52,7 +36,6 @@ module riscv(ram_link, rom_link, clk_in, led1, led2, RxD, TxD);
   core CORE(.ram_adress(ram_adress), .rom_adress(rom_adress), .data_in_ram(ram_read), .data_out_ram(ram_write), .data_in_rom(rom_read), .ram_enable_write(ram_write_enable), .vga_link(cpu_vga_link), .clk(clk));
   ramctlr RAM(.ram_link(ram_link), .cpu_link_adress(ram_adress), .cpu_link_read(ram_read), .cpu_link_write(ram_write), .write_enable(ram_write_enable), .clk(clk));
   romctlr ROM(.rom_link(rom_link), .cpu_link_read(rom_read), .cpu_link_adress(rom_adress), .clk(clk));
->>>>>>> Stashed changes
   vgactlr VGA(cpu_vga_link, led1, clk);
   clockctlr CLK(.clk_in(clk_in), .clk_out(clk));
 
